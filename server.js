e72d731fd4a81e6132e695183028f3e143e8a097
schar724 <scotty.charles@gmail.com>
2023-04-25 17:29:55 -0600

    css connected

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8000;
+const path = require("path");
+const port = process.env.PORT || 6600;
 
 app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname + "/public")));
 
 app.get("/", function (req, res) {
   res.render("pages/home");
